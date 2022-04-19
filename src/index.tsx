464497f4c545f985a105e1af4edfe6105deba4fd
crzoukman <zoukman@yandex.ru>
2022-04-19 23:24:29 +0300

    fix: migrate react to the latest version. remove console error

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App/App';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement);
+
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 );
